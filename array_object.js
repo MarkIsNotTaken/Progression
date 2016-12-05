d4b0af7c0fdae0931ee0eabf0db3ca3d85b14bf3
MarkIsNotTaken <mdemesillo@gmail.com>
2016-12-05 08:45:28 -1000

    array_object.js

--- array_object.js
+++ array_object.js
@@ -3,5 +3,20 @@
 	Mark Demesillo
 	Array And Objects
 	Dec. 5, 2016
-	
+
 */
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggins", "Ohai", "Jeff"];
+var colors = ["Light Black", "Ron Burandy", "Highlighter"];
+var adj = ["Smart", "Special Case", "Yummy", "Tiny Whiny", "Ferocious"];
+var actions = ["Scrap", "Kobe Beef", "Da kine", "Li dat'", "Pau Hana"];
+
+
+var donut = {
+	toppings : ["Sprinkles", "Chocolate Frosting", "o'hare air"],
+	filling : "Jelly",
+	shape : "Square",
+	count : 12
+}
+
